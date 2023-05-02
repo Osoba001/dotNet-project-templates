1cac36e1eec72c1d80c98cb6eb8e6ebc51e7e6b1
osoba001 <sirkellyc@gmail.com>
2023-05-02 09:44:56 +0100

    mock option<AuthConfData> and unit test AuthSetup

--- Test/Modules/AuthModule.Test/AuthApplication/AuthSetup/AuthSetupTests.cs
+++ Test/Modules/AuthModule.Test/AuthApplication/AuthSetup/AuthSetupTests.cs
@@ -11,15 +11,23 @@ namespace AuthModule.Test.AuthApplication.AuthSetup
         private readonly Mock<IUserRepo> _userRepoMock;
         private readonly IOptions<AuthConfigData> _configData;
         private readonly Auth.Application.AuthServices.AuthSetup _authSetup;
+        
         public AuthSetupTests()
         {
             _userRepoMock = new Mock<IUserRepo>();
-            _configData = new Mock<IOptions<AuthConfigData>>().Object;
+            AuthConfigData authData=new AuthConfigData() {
+                AccessTokenLifespanInMins = 20,
+                RefreshTokenLifespanInMins=30,
+                SecretKey="dhufjijkfooblfl;pdpff0rf85-jbm3-8y"
+            };
+            var mock = new Mock<IOptions<AuthConfigData>>();
+            mock.Setup(m=>m.Value).Returns(authData);
+            _configData=mock.Object;
             _authSetup = new Auth.Application.AuthServices.AuthSetup(_userRepoMock.Object, _configData);
         }
 
         [Fact]
-        public void PasswordManager_Should_Hash_Password()
+        public void PasswordManager_Should_HashPassword()
         {
             // Arrange
             var password = "password";
@@ -34,7 +42,7 @@ namespace AuthModule.Test.AuthApplication.AuthSetup
         }
 
         [Fact]
-        public void VerifyPassword_Should_Return_True_If_Password_Matches()
+        public void VerifyPassword_ReturnTrue_WhenPasswordMatches()
         {
             // Arrange
             var password = "password";
@@ -49,7 +57,7 @@ namespace AuthModule.Test.AuthApplication.AuthSetup
         }
 
         [Fact]
-        public void VerifyPassword_Should_Return_False_If_Password_Does_Not_Match()
+        public void VerifyPassword_ReturnFalse_WhenPasswordDoesNotMatch()
         {
             // Arrange
             var password = "password";
@@ -63,24 +71,24 @@ namespace AuthModule.Test.AuthApplication.AuthSetup
             result.Should().BeFalse();
         }
 
-        //[Fact]
-        //public async Task TokenManager_Should_Return_Tokens_And_Update_User()
-        //{
-        //    // Arrange
-        //    var user = Users[0];
-        //    _userRepoMock.Setup(x => x.Update(It.IsAny<UserModel>()))
-        //        .ReturnsAsync(new Utilities.Responses.KOActionResult());
+        [Fact]
+        public async Task TokenManager_Should_Return_Tokens_And_Update_User()
+        {
+            // Arrange
+            var user = Users[0];
+            _userRepoMock.Setup(x => x.Update(It.IsAny<UserModel>()))
+                .ReturnsAsync(new Utilities.Responses.KOActionResult());
 
-        //    // Act
-        //    var result = await _authSetup.TokenManager(user);
+            // Act
+            var result = await _authSetup.TokenManager(user);
 
-        //    // Assert
-        //    result.Should().NotBeNull();
-        //    result.AccessToken.Should().NotBeNullOrEmpty();
-        //    result.RefreshToken.Should().NotBeNullOrEmpty();
-        //    user.RefreshToken.Should().NotBeNullOrEmpty();
-        //    user.RefreshTokenExpireTime.Should().BeCloseTo(DateTime.UtcNow.AddMinutes(_configData.Value.RefreshTokenLifespanInMins),TimeSpan.FromSeconds(1));
-        //    _userRepoMock.Verify(x => x.Update(user), Times.Once);
-        //}
+            // Assert
+            result.Should().NotBeNull();
+            result.AccessToken.Should().NotBeNullOrEmpty();
+            result.RefreshToken.Should().NotBeNullOrEmpty();
+            user.RefreshToken.Should().NotBeNullOrEmpty();
+            user.RefreshTokenExpireTime.Should().BeCloseTo(DateTime.UtcNow.AddMinutes(_configData.Value.RefreshTokenLifespanInMins), TimeSpan.FromSeconds(1));
+            _userRepoMock.Verify(x => x.Update(user), Times.Once);
+        }
     }
 }
